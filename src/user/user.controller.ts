8d5e7fd6133866f257bc60cd3cac4451819ee7d3
Lucas <lucassocorrosilva@gmail.com>
2023-03-25 21:55:41 -0300

    Add restrições nas rotas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,6 +10,7 @@ import { Role } from "src/enums/roles.enum";
 import { RoleGuard } from "src/guards/role.guard";
 import { AuthGuard } from "src/guards/auth.guard";
 
+@Roles(Role.Admin)
 @UseGuards(AuthGuard,RoleGuard)
 @UseInterceptors(LogInterceptor) 
 @Controller('users')
@@ -18,37 +19,31 @@ export class UserController {
 
     constructor(private readonly userService: UserService){}
 
-@Roles(Role.Admin)
 @Post()
     async create(@Body() data: CreateUserDTO){
         return this.userService.create(data);
     }
 
-@Roles(Role.Admin)
 @Get()
     async list(){
         return this.userService.list();
     }
 
-@Roles(Role.Admin)
 @Get(':id')
     async show(@ParamId() id: number ){
         return this.userService.show(id);
     }
 
-@Roles(Role.Admin)
 @Put(':id')
     async update(@Body() data: UpdatePutUserDTO,@ParamId() id: number ){
         return this.userService.update(data, id)
     }
 
-@Roles(Role.Admin)
 @Patch(':id')
  async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number ){
     return this.userService.updatePartial(data, id);
  }
 
-@Roles(Role.Admin)
 @Delete(':id')
  async destroy(@ParamId() id: number ) {
     return this.userService.destroy(id);
