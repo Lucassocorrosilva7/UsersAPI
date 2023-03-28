cb645248e7fa34e7993795d27b4f9387d8c5abb4
Lucas <lucassocorrosilva@gmail.com>
2023-03-27 23:53:35 -0300

    Add ThrottlerModule e config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+  ThrottlerModule.forRoot({
+    ttl: 60,
+    limit: 10
+  }),
+  forwardRef(() => UserModule), 
+  forwardRef(() => AuthModule)],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService]
