b61ee708e909ab9d7f4b202eb3bc19b150adb012
Lucas <lucassocorrosilva@gmail.com>
2023-03-29 17:51:01 -0300

    Criando controller de upload de imagens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,24 @@
-import { Body, Controller, Post, Headers, Header, UseGuards, Req } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { AuthLoginDTO } from "./dto/auth-login.dto";
 import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
 import { UserService } from "src/user/user.service";
 import { AuthService } from "./auth.service";
-import { AuthMeDTO } from "./dto/auth-me.dto";
 import { AuthGuard } from "src/guards/auth.guard";
 import { User } from "src/decorators/user.decorator";
-
-
+import { FileInterceptor } from "@nestjs/platform-express";
+import { join } from "path";
+import { FileService } from "src/file/file.service";
 
 @Controller('auth')
 export class AuthController {
 
     constructor(
         private readonly userService: UserService,
-        private readonly authService: AuthService
+        private readonly authService: AuthService,
+        private readonly fileService: FileService,
+
     ) { }
 
     @Post('login')
@@ -42,7 +44,22 @@ export class AuthController {
     @UseGuards(AuthGuard)
     @Post('me')
     async me(@User() user) {
-        return {user}
+        return { user }
+    }
+
+    @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(AuthGuard)
+    @Post('photo')
+    async uploadPhoto(@User() user, @UploadedFile() photo: Express.Multer.File) {
+
+        const path = join(__dirname, '..', '..', 'storage', 'photos', `photos-${user.id}.png`)
+
+        try {
+            await this.fileService.upload(photo, path);
+        } catch (error) {
+            throw new BadRequestException(error)
+        }
+        return { sucess: true };
     }
 }
 
