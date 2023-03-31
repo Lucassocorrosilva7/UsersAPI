74ce8d03b4238d21c3f10dfb2bf0ade4f6a63666
Lucas <lucassocorrosilva@gmail.com>
2023-03-31 13:37:17 -0300

    Add config MailerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
+
 
 @Module({
   imports: [
@@ -15,7 +19,21 @@ import { UserModule } from './user/user.module';
       limit: 100,
     }),
     forwardRef(() => UserModule),
-    forwardRef(() => AuthModule)],
+    forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: 'smtps://emely.ebert22@ethereal.email:1JSPQDK9C6rQSBFkKg@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
